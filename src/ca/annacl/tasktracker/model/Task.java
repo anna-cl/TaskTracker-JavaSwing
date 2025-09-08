8fa7dada84d306b083f58a457f69a092c7d6b364
Anna <anna_liang_2@sfu.ca>
2025-09-07 22:41:04 -0700

    Remove tracked .DS_Store and .vscode

--- src/ca/annacl/tasktracker/model/Task.java
+++ src/ca/annacl/tasktracker/model/Task.java
@@ -1,5 +1,39 @@
 package ca.annacl.tasktracker.model;
 
+import java.util.GregorianCalendar;
+
 public class Task {
-    
+    String taskName;
+    String notes;
+    GregorianCalendar dueDate;
+    Boolean completed;
+
+    public Task(){
+        completed = false;
+    }
+
+    public String getTaskName(){
+        return taskName;
+    }
+
+    public String getNotes(){
+        return notes;
+    }
+
+    public GregorianCalendar getDueDate(){
+        return dueDate;
+    }
+
+    public boolean isCompleted(){
+        return completed;
+    }
+
+    public void setCompleted(boolean completed){
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString(){
+        return "" ;
+    }
 }
